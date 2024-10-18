302f3879d2b17fd14fdaa4bbe79593f6d7476e1f
wobnets <9gy43end@anonaddy.me>
2024-10-18 13:13:40 -0400

    fuck background

--- extension/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  const defaultUsername = 'USERNAME_PLACEHOLDER';
-  const defaultPassword = 'PASSWORD_PLACEHOLDER';
-
-  chrome.storage.local.set({ username: defaultUsername, password: defaultPassword }, () => {
-    console.log("Default credentials set");
-  });
-});
