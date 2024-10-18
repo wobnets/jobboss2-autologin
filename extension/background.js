d4fd38da5f9d3215f5f7cb18e83bb12f1753ea6e
wobnets <9gy43end@anonaddy.me>
2024-10-18 13:00:22 -0400

    storage

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,8 @@
 chrome.runtime.onInstalled.addListener(() => {
-  // Set default values for username and password
-  chrome.storage.sync.set({ username: 'USERNAME_PLACEHOLDER', password: 'PASSWORD_PLACEHOLDER' }, () => {
-    console.log("Default credentials set.");
+  const defaultUsername = 'USERNAME_PLACEHOLDER';
+  const defaultPassword = 'PASSWORD_PLACEHOLDER';
+
+  chrome.storage.sync.set({ username: defaultUsername, password: defaultPassword }, () => {
+    console.log("Default credentials set:", { username: defaultUsername, password: defaultPassword });
   });
 });
